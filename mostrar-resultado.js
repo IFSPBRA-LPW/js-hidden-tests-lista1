0594987dfde84cc1a3ef7197278014edb54b4b3b
Ana Paula Martins <martins.ana@ifsp.edu.br>
2026-03-12 08:47:05 -0300

    Create mostrar-resultado.js

---
+++ mostrar-resultado.js
@@ -0,0 +1,21 @@
+const fs = require("fs")
+
+const dados = JSON.parse(fs.readFileSync("result.json"))
+
+console.log("=================================")
+console.log("RESULTADO DOS EXERCÍCIOS")
+console.log("=================================\n")
+
+let contador = 1
+
+dados.testResults.forEach(suite => {
+
+    suite.assertionResults.forEach(teste => {
+
+        const status = teste.status === "passed" ? "✔" : "✘"
+
+        console.log(`Exercício ${contador} ${status}`)
+
+        contador++
+
+    })
